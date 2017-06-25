3b5cdbfc7ac7dce562e2dcdd0640c7fa800c661d
Jimmy Tsao <jimmy.tsao@gmail.com>
2017-06-25 18:10:39 -0500

    Refactor entry to use require-dir

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-var scss = require('./tasks/1n-scss');
-var bundlejs = require('./tasks/1n-bundlejs');
-var copy = require('./tasks/1n-copy');
-var sequence = require('./tasks/1n-sequence');
-var watch = require('./tasks/1n-watch');
-var del = require('./tasks/1n-delete');
-var browserSync = require('./tasks/1n-browser-sync');
-var nodemon = require('./tasks/1n-nodemon');
-var custom = require('./tasks/1n-custom');
-
 var _ = require('lodash');
+var requireDir = require('require-dir');
 
-var taskSetupMap = {
-  '1n-scss': scss,
-  '1n-bundlejs': bundlejs,
-  '1n-copy': copy,
-  '1n-sequence': sequence,
-  '1n-watch': watch,
-  '1n-delete': del,
-  '1n-browser-sync': browserSync,
-  '1n-nodemon': nodemon,
-  '1n-custom': custom
-};
+var taskSetupMap = requireDir('./tasks');
 
 module.exports = function(config){
 
